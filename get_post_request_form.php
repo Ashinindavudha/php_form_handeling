9b94c69aaca86ff8d46dae8e315910b4bfe7d67b
AshinIndavudha <ashinindavudha2018@gmail.com>
2020-05-21 16:48:07 +0630

    PHP_Form_Handeling_21_5_2020_AshinIndavudha

---
+++ get_post_request_form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Get Form</title>
+</head>
+<body>
+	<?php require_once 'get_post_request_process.php' ?>
+	<form action="" method="POST">
+		<p>First Name: <input type="text" name="name" value=""></p>
+		<p>Last Name: <input type="text" name="lastname" value=""></p>
+		<p>Email: <input type="email" name="email" value=""></p>
+		<input type="submit" name="submit" value="Submit">
+	</form>
+	<br><hr>
+	<div>
+		<a href="https://www.facebook.com/rose.tr.75" target="_blank" style="text-decoration: none;">Created By Ashin Indavudha</a>
+	</div>
+</body>
+</html>
